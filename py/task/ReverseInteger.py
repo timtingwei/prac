505d918cde1c47c8bd568eabebbbfc7b11fa3fc0
Tim Hu <timtingwei@gmail.com>
2016-08-09 21:10:34 +0800

    160809_Leecode_RI101

--- py/task/ReverseInteger.py
+++ py/task/ReverseInteger.py
@@ -18,18 +18,44 @@ class Solution(object):
         if x<0:
             list_s = list_s[:len(list_s)-1]
         print (list_s)
-        string = ''
-        for i in list_s:
-            string += str(i)
-        fina_s = int(string)
-        print (fina_s)
+        sum = 0
+        for i in range(len(list_s)):
+            sum = sum*10+int(list_s[i])
+
+        print (sum)
+
 s = Solution()
 s.reverse(-1223)
 
+class OtherSolution(object):
+    def reverse(self,x):
+        """
 
-"""
-L = ['1','2','3']
-l = reversed(L)
-print (list(l))
-print (list(str(123)))
-"""
+        :param x: int
+        :return: int
+        """
+        s = 0
+        if x<0:
+            s = 1
+            x = -x
+        if x==0:
+            return 0
+        n = x
+        num = []
+        while n>0:
+            num.append(n%10)
+            n //=10
+        sum = 0
+        for j in range(len(num)):
+            sum = sum*10+num[j]
+
+        if sum>2147483647:
+            return 0
+        if (s==1):
+            return -sum
+        return sum
+
+s2 = OtherSolution()
+print (s2.reverse(-1234))
+
+
