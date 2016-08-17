21d8f9145f12902c8391360c1387e4d7d93ca400
Tim Hu <timtingwei@gmail.com>
2016-08-17 12:39:17 +0800

    revise reduce function

--- py/task/cxt_18.py
+++ py/task/cxt_18.py
@@ -1,6 +1,7 @@
 #cxt_18.py
 
 #求s=a+aa+aaa+aaaa+aa...a的值，其中a是一个数字。例如2+22+222+2222+22222(此时共有5个数相加)，几个数相加有键盘控制。
+from functools import reduce
 
 def superAdd(num,count=1):
     #至少有一个数相加
@@ -44,7 +45,6 @@ for count in range(n):
     Tn = Tn + a
     a = a * 10
     Sn.append(Tn)
-    print (Tn)
+print (reduce(lambda x,y:x+y,Sn))
 
-#Sn = reduce(lambda x,y:x+y,Sn)
-#print (Sn)
+
