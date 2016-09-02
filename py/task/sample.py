a90feafaf83398475f41c8f1506b975157134124
Tim Hu <timtingwei@gmail.com>
2016-09-03 00:24:14 +0800

    update sample

--- py/task/sample.py
+++ py/task/sample.py
@@ -11,13 +11,23 @@ print ('%s = %f' %(lst_join,sum(lst)))
 
 n = 1
 sum = 0
-iter = 100
+iter =  100
 s = ''
 while n<iter+1:
     sum = sum + n
-    if n == 100:
+    if n == 99:
         s = s+str(n)
     else:
         s = s+str(n)+'+'
     n += 2
 print (s,'=',sum)
+
+a = 3
+if a==1:
+    print ('a=','1')
+elif a==2:
+    print ('a!=','2')
+else:
+    print ('a!= 1 or 2')
+
+
