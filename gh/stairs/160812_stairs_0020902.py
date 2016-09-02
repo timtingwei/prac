206d94560fbb930b939b2bfca0acf33d59e1895f
Tim Hu <timtingwei@gmail.com>
2016-09-03 00:42:14 +0800

    update function roughly,need to be test

--- gh/stairs/160812_stairs_0020902.py
+++ gh/stairs/160812_stairs_0020902.py
@@ -54,9 +54,6 @@ def settype(housingType=0):
         domain_t, domain_r, domain_tadd2r, commendSlope, basicSlope= (300,1000),(0,160),(550,650),7/11,7/11
     return domain_t,domain_r,domain_tadd2r,commendSlope,basicSlope
 
-    
-
-
 def getcount(stepsize,boxsize,platformWidth,epsilion):
     """
     Parameters:
@@ -88,6 +85,22 @@ def setBoxsize(length,width,height):
         
     """
     return length,width,height
+def getBoxsize(stepCount,stepHeight,stepWidth,platformWidth,stairsLength,distance):
+    """reset boxsize from other attribute
+    Returns:
+        boxsize:list,
+                [0]box_length
+                [1]box_weight
+                [2]box_height
+    """
+    #///重新给box的三个属性赋值
+    box_width = (stepCount-1)*stepWidth + platformWidth
+    box_length = (stairsLength*2+distance) 
+    #distance = box_length-stairsLength*2
+    box_height = stepCount*2*stepHeight
+    #print ("box_length:%s,box_width:%s,box_height:%s"%(box_length,box_width,box_height))
+    return box_length,box_width,box_height
+
 def weight4length(box_length,stairsLength,distance):
     """weighting value between box_length ,stairsLength and distance
     Parameters:
@@ -143,89 +156,18 @@ def convert2thick(stairsThickness,stepCount,stepWidth,box_height):
     thick = stairsThickness/slope_cos
     return thick
 
-try:
-    #print (settype())
-    #print (getStepsize(settype()[0],settype()[1],settype()[2],settype()[3],settype()[4])[0])
-    stepsize = getStepsize(settype()[0],settype()[1],settype()[2],settype()[3],settype()[4])[0]
-    boxsize = setBoxsize(box_length,box_width,box_height)
-    epsilion = 20
-    stepCountLst =  (getcount (stepsize,boxsize,platformWidth,epsilion))
-    #索引stepCountLst第一个值，得到踏步的尺寸和数量
-    stepCount = stepCountLst[0][0]
-    stepWidth = stepCountLst[0][1]
-    stepHeight = stepCountLst[0][2]
-    #print ("box_length:%s,box_width:%s,box_height:%s"%(box_length,box_width,box_height))
-    
-    box_length = weight4length(box_length,stairsLength,distance)[0]
-    stairsLenght = weight4length(box_length,stairsLength,distance)[1]
-    distance = weight4length(box_length,stairsLength,distance)[2] 
-    box_weight = boxsize[1]
-    box_height = boxsize[2]  
-    thick =  convert2thick(stairsThickness,stepCount,stepWidth,box_height)
-    
-    original =  boxsize
-    ultimate = setBoxsize(box_length,box_weight,box_height)
-    #compareError(original,ultimate)
-    print ('original:%s,ultimate:%s'%(original,ultimate))
-    print (compareError(original,ultimate))
-    l,w,h = compareError(original,ultimate) #///之前的boxsize与当前的setBoxsize()比较
+
+def weight4width(box_width,platformWidth,stairsLength,stepCount,stepWidth):
+    """weighting value between platformWidth and stairsLength
+    Returns:
+        plamformWidth:float
+    """
+    if (platformWidth-stairsLength)<0:
+        platformWidth = stairsLength + 50 #platform width must larger than stairs length ,the value is 50
+        return platformWidth
+    else:return platformWidth
 
 
-except IOError as e:
-    raise ('error:%s'% e)
-
-
-    
-    
-    
-#/// 创建一个收集实体的list
-objs = []
-
-#///记录下初始的box长宽高三属性,能够在后面反馈出误差
-init_l = box_length
-init_w = box_width
-init_h = box_height
-
-"""
-#///对stairsLength进行修正
-#///在distance>0的情况下，boxWidth改变对间距和楼梯宽的权重
-if (stairsLength*2+distance != box_length) and(distance>0):
-    dist = stairsLength*2+distance-box_length
-    #if distance>=0:
-    distance -=dist*0.4
-    stairsLength -=dist*0.3
-#///如果间距不存在了，给他一个固定的间距。正确情况是，box_length如果太小了，保持梯段的宽度，缩小间距   
-if (distance<=20):
-    distance = 500
-    stairsLength = (box_length-distance)/2
-    #///正确情况是，box_length如果太小了，保持梯段的宽度，缩小间距 
-    if stairsLength <=1000:
-        stairsLength = 1000
-        distance = (box_length-2*stairsLength)
-        #///如果这个时候间距也小于20了,程序不能运行
-        if distance<20:
-            distance = 20
-            print ("Error:%s"% "there is no distance betweeen stairs")
-"""
-#///根据平台宽度需要大于等于楼梯宽度的规范，进行函数制约       ##########Error#########
-def PlatformWidth():
-    pass
-
-
-
-#///重新给box的三个属性赋值
-box_width = (stepCount-1)*stepWidth + platformWidth
-box_length = (stairsLength*2+distance) 
-#distance = box_length-stairsLength*2
-box_height = stepCount*2*stepHeight
-#print ("box_length:%s,box_width:%s,box_height:%s"%(box_length,box_width,box_height))
-
-"""
-#///插写梯段厚的函数
-slope_cos = ((stepCount-1)*stepWidth)/math.sqrt((box_height*0.5)**2+((stepCount-1)*stepWidth)**2)#///坡度的余弦值
-thick = stairsThickness/slope_cos
-print (stepCount)
-"""
 
 def AddStairsPts(x_direction=False):
     """
@@ -273,8 +215,7 @@ def AddStairsPts(x_direction=False):
         pts.append(pt)
     return pts
 
-a = AddStairsPts(False)#第一个梯段
-b = AddStairsPts(True)#第二个梯段
+
 
 def StairsPolyLine(*ptLists):
     """
@@ -295,38 +236,38 @@ def StairsPolyLine(*ptLists):
         
     return pls   
 
-lines = StairsPolyLine(a,b)
 
-"""
-def boundaryExtrude(polyline,path): ###R###
-    surf = rs.AddPlanarSrf(polyline)
-    #path = rs.AddCurve([(0,stairsLength,0),(0,0,0)]) 
-    #polyline_brep = rs.coercebrep(polyline,True)
-    #polyline = rs.coercecurve(polyline,-1)
-    subs = rs.ExtrudeCurve(polyline,path) #//////版本问题 why rs.ExtrudeSurface can't be used
-    
-    return subs
-    
-#bou = boundaryExtrude(line)
-"""
-
-def ExtrudeStairs(polyline,bool):
-    #这个函数写成list的形式，有点面向过程，但先试试看,
-    #不能同时带入两个可变参
-    if bool == True:
-        path_stairs = rs.AddCurve([(0,0,0),(0,stairsLength,0)])
+def getpath(distance,direction=True):
+    """get path from basic direction and distance
+    Parameters:
+        distance:float,length of path
+        direction:boolean,
+                    [0] represent positive x-axis direction
+                    [1] represent nagative x-axis direction
+    Returns:
+        path,Guid The identifier of the new object if successful
+    """
+    if direction == True:
+        path = rs.AddLine([0,0,0],[0,distance,0])
     else:
-        path_stairs = rs.AddCurve([(0,stairsLength,0),(0,0,0)])
-    stairs = rs.ExtrudeCurve(polyline,path_stairs)
+        path = rs.AddLine([0,distance,0],[0,0,0])
+    return path
+def ExtrudeStairs(polyline,path):
+    """Extrude polyline along certain path
+    Parameters:
+        polyline:guid
+        path:guid
+    Returns:
+        stairs:guid,new object if successful
+    """
+    #print ('type poly:%s,type path:%s'%(type(polyline),type(path)))
+    surface = rs.AddPlanarSrf([polyline])
+    stairs = rs.ExtrudeSurface(surface,path,True)
     return stairs
 
-#stairsStep = ExtrudeStairs(lines,False,True)
-stairs1_step = ExtrudeStairs(lines[0],False)
-stairs2_step = ExtrudeStairs(lines[1],True)
-objs.append(stairs1_step)
-objs.append(stairs2_step)
 
-def platform():
+
+def platform(stepCount,stepWidth,stepHeight,stairsLength,beamWidth,beamHeight,platformThickness):
     #add platform points orderly
     
     pts_list = []
@@ -338,57 +279,69 @@ def platform():
     plat_pt6 = (plat_pt5[0]-platformWidth+2*beamWidth,-stairsLength,plat_pt5[2])
     plat_pt7 = (plat_pt6[0],-stairsLength,plat_pt3[2])
     plat_pt8 = (plat_pt1[0],-stairsLength,plat_pt3[2])###R###maybe creat list by range
-
-    pts_list.append(plat_pt1)
-    pts_list.append(plat_pt2)
-    pts_list.append(plat_pt3)
-    pts_list.append(plat_pt4)
-    pts_list.append(plat_pt5)
-    pts_list.append(plat_pt6)
-    pts_list.append(plat_pt7)
-    pts_list.append(plat_pt8)
-    pts_list.append(plat_pt1)
     
-    plat_pts = []
-    #print (pts_list)
-    for i in pts_list:
-        pt = rs.AddPoint(i)
-        plat_pts.append(pt)
+    pts_list.extend([plat_pt1,plat_pt2,plat_pt3,plat_pt4,plat_pt5,plat_pt6,plat_pt7,plat_pt8,plat_pt1])
+    plat_pts = [rs.AddPoint(n) for n in pts_list]
     
     #creat platform polyline
     platform_pl = rs.AddPolyline(plat_pts)
-    path_plat = rs.AddLine((0,0,0),(0,2*stairsLength+distance,0)) ####2*stairsLength + distance = platformLength写之间的限制函数
-    platform = rs.ExtrudeCurve(platform_pl,path_plat)
+    platform = ExtrudeStairs(platform_pl,getpath(2*stairsLength+distance,True))
     return platform
     
-plat = platform()
-objs.append(plat)
 
 
-def moveRotate():
+
+def moveRotate(objs,trans):
     rs.MoveObject(objs,trans)
     rs.RotateObjects(objs,trans,angle*360,(0,0,1))
     return objs
     
-moveRotate()
 
-"""
-#///反馈
-#width
-width = (stepCount-1)*stepWidth+platformWidth
-tol_w = init_w-width
-w = (width,tol_w)
+try:
+    #print (settype())
+    #print (getStepsize(settype()[0],settype()[1],settype()[2],settype()[3],settype()[4])[0])
+    stepsize = getStepsize(settype()[0],settype()[1],settype()[2],settype()[3],settype()[4])[0]
+    boxsize = setBoxsize(box_length,box_width,box_height)
+    epsilion = 20
+    stepCountLst =  (getcount (stepsize,boxsize,platformWidth,epsilion))
+    #索引stepCountLst第一个值，得到踏步的尺寸和数量
+    stepCount = stepCountLst[0][0]
+    stepWidth = stepCountLst[0][1]
+    stepHeight = stepCountLst[0][2]
+    #print ("box_length:%s,box_width:%s,box_height:%s"%(box_length,box_width,box_height))
+    
+    box_length = weight4length(box_length,stairsLength,distance)[0]
+    stairsLenght = weight4length(box_length,stairsLength,distance)[1]
+    distance = weight4length(box_length,stairsLength,distance)[2] 
+    box_width = boxsize[1]
+    box_height = boxsize[2]  
+    thick =  convert2thick(stairsThickness,stepCount,stepWidth,box_height)
+    
+    original =  boxsize
+    platformWidth = weight4width(box_width,platformWidth,stairsLength,stepCount,stepWidth)
+    ultimate = getBoxsize(stepCount,stepHeight,stepWidth,platformWidth,stairsLength,distance)
+    #compareError(original,ultimate)
+    print ('original:%s,ultimate:%s'%(original,ultimate))
+    print (compareError(original,ultimate))
+    l,w,h = compareError(original,ultimate) #///之前的boxsize与当前的setBoxsize()比较
 
-#box_length
-length = stairsLength*2+distance #///这里stairsLength distance应该是被权重之后的值
-tol_l = init_l-length
-l = (length,tol_l)
+    #/// 创建一个收集实体的list
+    objs = []
+    a = AddStairsPts(False)#第一个梯段
+    b = AddStairsPts(True)#第二个梯段
+    lines = StairsPolyLine(a,b)
+    print (lines)
+    #stairsStep = ExtrudeStairs(lines,False,True)
+    stairs1_step = ExtrudeStairs(lines[0],getpath(stairsLength,False))
+    stairs2_step = ExtrudeStairs(lines[1],getpath(stairsLength,True))
+    objs.append(stairs1_step)
+    objs.append(stairs2_step)
+    plat = platform(stepCount,stepWidth,stepHeight,stairsLength,beamWidth,beamHeight,platformThickness)
+    objs.append(plat)
+    moveRotate(objs,trans)
 
-#height
-height = stepCount*2*stepHeight
-tol_h = init_h-height
-h = (height,tol_h)
-"""
+except IOError as e:
+    raise ('error:%s'% e)
 
 """
 #///160813
@@ -397,4 +350,8 @@ h = (height,tol_h)
 把自己理解的和规范都写进代码里
 明确另外的几种情况:直行单跑楼梯，平行转梯，（旋转楼梯），折行双跑楼梯，折行旋转梯，折行三跑楼梯
 
+#///160902
+把一些过程整理成了函数，分解的过程被模块化。
+考虑调用是否可以再简洁，测试函数的功能是否完善
+插写另外的情况
 """
