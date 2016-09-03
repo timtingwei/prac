5ec8807006b7215a60040c28fc1876ceaaec9c41
Tim Hu <timtingwei@gmail.com>
2016-09-03 20:26:03 +0800

    160903_py_sys_smaple

---
+++ py/simplePy/using_sys.py
@@ -0,0 +1,11 @@
+#Filename: using_sys.py
+
+import sys
+print (sys.argv)
+print ('The command line arguments are:')
+for i in sys.argv:
+    print (i)
+
+print ('\n\n The PYTHONEPATH is ',sys.path,'\n')
+
+print (sys.argv)
