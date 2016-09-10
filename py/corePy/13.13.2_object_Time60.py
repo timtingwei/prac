41d674f8f728d91c6e43fcddf5c667168f875eed
Tim Hu <timtingwei@gmail.com>
2016-09-10 08:14:11 +0800

    __add__() and __iadd__()

--- py/corePy/13.13.2_object_Time60.py
+++ py/corePy/13.13.2_object_Time60.py
@@ -26,3 +26,54 @@ mon = Time60(10,30)
 tue = Time60(11,15)
 print (mon,tue)
 #10 : 30 11 : 15
+
+print (mon.__class__)
+#<class '__main__.Time60'>   ==> 相当于Time60()
+
+class Time60(object):
+    def __init__(self,hr,min):
+        self.hr = hr
+        self.min = min
+
+    def __str__(self):
+        return '%d : %d'%(self.hr,self.min)
+
+    def __add__(self,other): #//MODO: 覆盖原本的__add__()函数，
+        return self.__class__(self.hr + other.hr ,self.min + other.min)   #MODO: 在类中一般用 self.__class__ 调用类名，而不直接用类名调用。不需要自己多加思考。
+
+    __repr__ = __str__    
+
+mon = Time60(10,30)
+tue = Time60(11,15)
+wed = mon + tue
+print (wed)
+thu = Time60(11,44)
+print (wed + thu )
+#32 : 89
+
+#///MODO： 原位相加
+
+class Time60(object):
+    def __init__(self,hr,min):
+        self.hr = hr
+        self.min = min
+
+    def __str__(self):
+        return '%d : %d'%(self.hr,self.min)
+
+    def __add__(self,other): #//MODO: 覆盖原本的__add__()函数，
+        return self.__class__(self.hr + other.hr ,self.min + other.min)   #MODO: 在类中一般用 self.__class__ 调用类名，而不直接用类名调用。不需要自己多加思考。
+
+    __repr__ = __str__  
+
+    def __iadd__(self,other):
+        self.hr += other.hr
+        self.min += other.min
+        return self
+
+mon = Time60(10,30)
+tue = Time60(11,15)
+print (mon + tue)
+tue += mon
+print (tue)
+#21 : 45
