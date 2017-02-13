2f8cb5a394448d78447628a1c01771699d409328
Tim Hu <timtingwei@gmail.com>
2017-02-13 10:53:02 +0800

    Timeout

--- py/scrapy/urllib2_test15.py
+++ py/scrapy/urllib2_test15.py
@@ -4,3 +4,9 @@ import urllib2
 import socket
 socket.setdefaulttimeout(10) #10秒钟后超时
 urllib2.socket.setdefaulttimeout(10) #另一种方式
+#以上做法是Python2.6之前
+
+#Python2.6之后,超时可以通过urllib2.urlopen()直接设置
+import urllib2
+response = urllib2.urlopen('http://www.google.com',timeout = 10)
+
