fb4df919b545151d084de3aa97edf6dd58150cf8
Tim Hu <timtingwei@gmail.com>
2017-02-13 09:15:22 +0800

    URLError

--- py/scrapy/urllib2_test07.py
+++ py/scrapy/urllib2_test07.py
@@ -14,3 +14,13 @@ try:
 
 except urllib2.URLError as e:
     print e.read()
+
+"""
+<html>
+<head><title>403 Forbidden</title></head>
+<body bgcolor="white">
+<center><h1>403 Forbidden</h1></center>
+<hr><center>nginx</center>
+</body>
+</html>
+"""
