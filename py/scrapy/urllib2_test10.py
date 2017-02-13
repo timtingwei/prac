db9e291a98a9f626294e11e971b3d00cb0020d5e
Tim Hu <timtingwei@gmail.com>
2017-02-13 09:55:10 +0800

    geturl()

--- py/scrapy/urllib2_test10.py
+++ py/scrapy/urllib2_test10.py
@@ -16,4 +16,4 @@ print 'Old url : ' + old_url
 print 'Real url : ' + response.geturl()
 #Old url : http://weibo.com/u/3182555220?refer_flag=1001030201_
 #Real url : https://passport.weibo.com/visitor/visitor?entry=miniblog&a=enter&url=http%3A%2F%2Fweibo.com%2Fu%2F3182555220%3Frefer_flag%3D1001030201_&domain=.weibo.com&ua=php-sso_sdk_client-0.6.23&_rand=1486949154.7018
-#你
+#followed a redirect
