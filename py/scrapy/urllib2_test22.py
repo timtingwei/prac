c7d1ee8525794c5a04c547ff6ddb3f479431165d
Tim Hu <timtingwei@gmail.com>
2017-02-13 13:25:51 +0800

    POST

---
+++ py/scrapy/urllib2_test22.py
@@ -0,0 +1,22 @@
+#urllib2_test22.py
+#-*- coding:utf-8 -*-
+
+#表单的处理 
+import urllib
+import urllib2
+
+postdata = urllib.urlencode({
+    'username':'汪小光',
+    'password':'why888',
+    'continueURI':'http://www.verycd.com/',
+    'fk':'',
+    'login_submit':'登录'
+})
+
+req = urllib2.Request(
+    url = 'http://secure.verycd.com/signin',
+    data = postdata
+)
+
+result = urllib2.urlopen(req)
+print result.read()
