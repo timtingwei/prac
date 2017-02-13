5c21cf270b8f36c636dada4ced8484e850c5cd0d
Tim Hu <timtingwei@gmail.com>
2017-02-14 06:35:34 +0800

    subn

---
+++ py/scrapy/re11.py
@@ -0,0 +1,19 @@
+#re11.py
+
+#subn
+
+import re
+
+p = re.compile(r'(\w+) (\w+)')
+s = 'i say, hello world!'
+
+print p.subn(r'\2 \1', s)
+
+def func(m):
+    return m.group(1).title() + ' ' + m.group(2).title()
+
+print p.subn(func,s)
+
+### output ###
+#('say i, world hello!', 2)
+#('I Say, Hello World!', 2)
