9a5a1fea5d500f65211f408b0222dd0c1b4323ac
timtingwei <timtingwei@hotmail.com>
2017-02-26 14:49:20 +0800

    fullfill version 0.1

--- geo/convex_hull/slow_convex_hul.py
+++ geo/convex_hull/slow_convex_hul.py
@@ -1,4 +1,5 @@
 #-*- coding:utf-8 -*-
+#version 0.1
 import random
 
 def SlowConvexHull(P):
@@ -17,23 +18,16 @@ def SlowConvexHull(P):
                     if (q != j) and (q != i):
                         p_list.append(q)
                 rest_index.append(p_list)
-    #print 'rest_index : ' , rest_index
-    #print 'p_pairs:' , p_pairs
-    #print 'len(p_pairs):%d,len(rest_index):%d',len(p_pairs),len(rest_index)
-    L = []
-    for line in p_pairs:
+    edge_list = []
+    for line in p_pairs:                                               #add line which other points in right side  ---> clockwise
         valid = True
-        #for all points not equal p or q
-        for pt_index in rest_index[p_pairs.index(line)]:
-            print 'P[pt_index]:',P[pt_index]
-            print 'line:',line
+        for pt_index in rest_index[p_pairs.index(line)]:               #for all points not equal p or q
             valid = impilicit_form(P[pt_index],line)
             if valid == False:
                 break
         if valid == True:
-            L.append(line)
-    print 'P:',P
-    print 'L:', L
+            edge_list.append(line)
+    return edge_list
     
 def GeneratePoint(n):
     #input:number of points
@@ -41,36 +35,54 @@ def GeneratePoint(n):
     domain = [-5,5]
     P = []
     for i in range(n):
-        P.append([random.randint(-5,5) , random.randint(-5,5) ])
+        P.append([random.random()*5 + -5  , random.random()*5 + -5 ])
     return P
 
-def givePoints():
-    P = [[-2, 0], [2, 0], [4, 4], [-5, 5], [-2, 3]]
-    return P
-def impilicit_form(point,line):                                        ### judgement side of point wrong
+def impilicit_form(point,line):                                        ### fullfill judgement of which side
     vectorLine = [line[1][i]-line[0][i] for i in range(len(line[0]))]
     normal = [-vectorLine[1], vectorLine[0]]
     distance = line[0][0] * normal[0] + line[0][1] * normal[1]
-    #print 'distance : ',distance
-    #print 'normal : ',normal
-    #print 'point : ' , point
     dot = sum ([ ( point[i] * normal[i]) for i in range(len(point))]) - distance 
-    #print 'dot : ',dot
-    if dot > 0 :                                                       #left ---> False  
-        print 'positive'
+    if dot > 0 :                                                       #left ---> False  if vec_n*X - d > 0 ---> positive  
         return False 
-    elif dot < 0:                                                      #right ---> True
-        print 'negative'
+    elif dot < 0:                                                      #right ---> True ---> negative
         return True
-    else: 
-        print 'on line'
+    else:                                                              #dot = 0 ---> on line 
         return True
 
+def sort_vertices(edge_list,L):
+    #from the setE of edges construct a list L of vertices of CH(P),sorted in clockwise order
+    last_vertex = L[-1]
+    for edge in edge_list:
+        if edge[0] == last_vertex:
+            L.append(edge[1])
+            #print edge[1]
+            edge_list.remove(edge)
+    if len(edge_list) > 1:
+        sort_vertices(edge_list,L)
+    return L
 
 def start():
-    #P = GeneratePoint(5)
-    P = [[-2, 0], [2, 0], [4, 4], [-5, 5], [-2, 3]]
-    L = SlowConvexHull(P)
+    P = GeneratePoint(5)
+    #P = [[-2, 0], [2, 0], [4, 4], [-5, 5], [-2, 3]]
+    print 'P:',P
+    edge_list = SlowConvexHull(P)
+    L = []
+    L.append(edge_list[0][0])
+    L.append(edge_list[0][1])
+    edge_list.pop(0)
+    L = sort_vertices(edge_list,L)
+    print 'L:',L
+
+    #write to file then drawing in rhino
+    f1 = open('E:/tim/prac/geo/convex_hull/L.txt','w')
+    f1.write(str(L))
+    f1.close()
+
+    f2 = open('E:/tim/prac/geo/convex_hull/P.txt','w')
+    f2.write(str(P))
+    f2.close()
+    
 
 start()
 
