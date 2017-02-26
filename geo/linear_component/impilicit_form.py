4a29568ab246d14e324dc71671b4a3f6c37a72f2
timtingwei <timtingwei@hotmail.com>
2017-02-26 12:17:21 +0800

    revise line to pts_on_line

--- geo/linear_component/impilicit_form.py
+++ geo/linear_component/impilicit_form.py
@@ -1,7 +1,13 @@
 
-def impilicit_form(point,line):
-    normal = [-line[1], line[0]]
-    dot = sum ([ ( point[i] * normal[i]) for i in range(len(point))])
+def impilicit_form(point,pts_on_line):
+    #vec_n = (-line[1],line[0])
+    #X =  [point[0],point[1]]
+    #d =  point_on_line[0] * vec_n[0] +  point_on_line[1] * vec_n[1]   
+    #if vec_n*X - d > 0 ---> positive
+    normal = [- (pts_on_line[1][1]-pts_on_line[0][1]), ( pts_on_line[1][0] - pts_on_line[0][0]) ]
+    distance =  normal[0] * pts_on_line[0][0] + normal[1] * pts_on_line[0][1]
+    dot = sum ([ ( point[i] * normal[i]) for i in range(len(point))]) +distance
+    print 'dot :',dot
     if dot > 0 : 
         print 'positive'
         return 0 
@@ -15,8 +21,8 @@ def impilicit_form(point,line):
 def start2():
 
     point = [2,0]
-    line = [9,-1]
-    impilicit_form(point,line)
+    pts_on_line = [[1,2],[-1,3]]
+    impilicit_form(point,pts_on_line)
 
 start2()
     
