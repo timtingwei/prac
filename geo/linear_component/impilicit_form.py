d11ef77c61f971580b8e9defaf842e42aa4d8a75
timtingwei <timtingwei@hotmail.com>
2017-02-27 13:16:44 +0800

    update judge_whichSide

--- geo/linear_component/impilicit_form.py
+++ geo/linear_component/impilicit_form.py
@@ -18,11 +18,30 @@ def impilicit_form(point,pts_on_line):
         print 'on line'
         return 2
 
+def judge_whichSide(target_pt,point0_line,point1_line):
+    vecLine = [point1_line[0] - point0_line[0] , point1_line[1] -point0_line[1]]
+    normal = [-vecLine[1] ,vecLine[0] ]
+    vec_p2x = [target_pt[0] - point0_line[0] , target_pt[1] - point0_line[1]]                      #vector point on line to target point
+    result = normal[0] * vec_p2x[0] + normal[1] *  vec_p2x[1]
+    print  result
+    if result > 0 : 
+        print 'judge_whichSide : positive'
+        return 0 
+    elif result < 0:
+        print 'judge_whichSide : negative'
+        return 1
+    else: 
+        print 'judge_whichSide : on line'
+        return 2
+
 def start2():
 
     point = [2,0]
     pts_on_line = [[1,2],[-1,3]]
     impilicit_form(point,pts_on_line)
+
+    judge_whichSide(point,pts_on_line[0],pts_on_line[1])
+
 
 start2()
     
